dad06f6d43c6353c839c0367fb94c9765a90ab43
Sarah Osgood <sarahjs117@gmail.com>
2020-12-01 18:33:55 -0700

    Add css styling to the movie container component

--- src/MovieCard/MovieCard.js
+++ src/MovieCard/MovieCard.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import './MovieCard.css';
 
 const MovieCard = ( {image, title, rating}) => {
   return (
     <section className="movie-card">
-      <img className="card-image" src={image}/>
-      <h2 className="card-title">Movie Title: {title}</h2>
+      <div className="image-holder">
+        <img className="card-image" src={image}/>
+      </div>
+      <h3 className="card-title">Movie Title: {title}</h3>
       <h3 className="rating">Movie Rating: {rating}</h3>
     </section>
   )

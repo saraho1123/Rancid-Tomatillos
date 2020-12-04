bafb0e706ab8ac3211b157a702142f7e05155e26
Sarah Osgood <sarahjs117@gmail.com>
2020-12-03 19:23:40 -0700

    Create functional component to render single moovie view for when a movie is clicked from the main view

--- src/SingleMovie/SingleMovie.js
+++ src/SingleMovie/SingleMovie.js
@@ -1,11 +1,40 @@
-import { render } from '@testing-library/react';
 import React from 'react'
 import './SingleMovie.css';
 
-const SingleMovie = () => {
+const SingleMovie = ({ movieDetails }) => {
   return (
-    <p>My mom steals silverware</p>
+    <section className="single-movie">
+      <div className="image-holder">
+        <img className="movie-image" src={movieDetails.poster_path} alt={movieDetails.title}/>
+      </div>
+      <h3 className="card-title">Movie Title: {movieDetails.title}</h3>
+      <h3 className="rating">Movie Rating: {Math.round( movieDetails.average_rating * 10 ) / 10}</h3>
+      <h3 className="release-date">Release Date: {movieDetails.release_date}</h3>
+      <h3 className="overview">Movie Overview: {movieDetails.overview}</h3>
+      <h3 className="genres">Genres: {movieDetails.genres}</h3>
+      <h3 className="budget">Movie Budget: {movieDetails.budget}</h3>
+      <h3 className="revenue">Movie Revenue: {movieDetails.revenue}</h3>
+      <h3 className="runTime">Movie Length: {movieDetails.runTime}</h3>
+      <h3 className="tagline">Movie Tagline: {movieDetails.tagline}</h3>
+    </section>
   )
 }
 
 export default SingleMovie;
+
+/*
+ <SingleMovie
+        id={movie.id}
+        key={movie.id}
+        image={movie.backdrop_path}
+        title={movie.title}
+        rating={movie.average_rating}
+        releaseDate={movie.release_date}
+        overview={movie.overview}
+        genres={movie.genres}
+        budget={movie.budget}
+        revenue={movie.revenue}
+        runTime={movie.runtime}
+        tagline={movie.tagline}
+      />
+*/

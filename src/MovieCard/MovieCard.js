aceb1231cd6d82a2e0d67be515d68c058baa809f
Sarah Osgood <sarahjs117@gmail.com>
2020-12-03 19:19:29 -0700

    Add additional props being passed from app component

--- src/MovieCard/MovieCard.js
+++ src/MovieCard/MovieCard.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './MovieCard.css';
 
-const MovieCard = ( {image, title, rating}) => {
+const MovieCard = ( {id, image, title, rating, displayMovieDetails}) => {
   return (
-    <section className="movie-card" onClick={ displayMovieDetails }>
+    <section className="movie-card" onClick={() => {displayMovieDetails(id)}  }>
       <div className="image-holder">
-        <img className="card-image" src={image}/>
+        <img className="card-image" src={image} alt={title}/>
       </div>
       <h3 className="card-title">Movie Title: {title}</h3>
       <h3 className="rating">Movie Rating: {Math.round( rating * 10 ) / 10}</h3>

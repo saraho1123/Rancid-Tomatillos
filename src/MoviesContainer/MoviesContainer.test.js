8a791ef6ea0e6220c951e30d4a67ca5f17e8c7a2
Chadrick Dickerson <chad.alan.dickerson@gmail.com>
2020-12-05 14:42:49 -0700

    Write pseudocode for MovieContainer

---
+++ src/MoviesContainer/MoviesContainer.test.js
@@ -0,0 +1,14 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import MovieContainer from './MovieContainer';
+
+describe('MovieContainer', () => {
+  it('should return movieCard on page load', () => {
+    // setup
+    // render the movieCard, with mocked const(s) of movies and mock functions of movieClicked displaySingleMovie
+    // execution
+    // 
+    // assertion
+    // expect the const to be in the document
+  })
+})

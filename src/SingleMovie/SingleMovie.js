97265b63aab1bd3737fc2e1f8deed0b8153cd001
Chadrick Dickerson <chad.alan.dickerson@gmail.com>
2020-12-04 18:51:13 -0700

    CSS styling in css, and attribute changes in js

--- src/SingleMovie/SingleMovie.js
+++ src/SingleMovie/SingleMovie.js
@@ -4,18 +4,20 @@ import './SingleMovie.css';
 const SingleMovie = ({ movieDetails, returnToHome }) => {
   return (
     <section className="single-movie">
-      <div className="image-holder">
-        <img className="movie-image" src={movieDetails.poster_path} alt={movieDetails.title}/>
+      <div className="backdrop-holder">
+        <img className="backdrop-image" src={movieDetails.backdrop_path} alt={movieDetails.title}/>
       </div>
-      <h3 className="card-title">Movie Title: {movieDetails.title}</h3>
-      <h3 className="rating">Movie Rating: {Math.round( movieDetails.average_rating * 10 ) / 10}</h3>
-      <h3 className="release-date">Release Date: {movieDetails.release_date}</h3>
-      <h3 className="overview">Movie Overview: {movieDetails.overview}</h3>
-      <h3 className="genres">Genres: {movieDetails.genres}</h3>
-      <h3 className="budget">Movie Budget: {movieDetails.budget}</h3>
-      <h3 className="revenue">Movie Revenue: {movieDetails.revenue}</h3>
-      <h3 className="runTime">Movie Length: {movieDetails.runTime}</h3>
-      <h3 className="tagline">Movie Tagline: {movieDetails.tagline}</h3>
+      <div className="movie-details-holder">
+        <h3 className="card-title">{movieDetails.title}</h3>
+        <h3 className="rating">Movie Rating: {Math.round( movieDetails.average_rating * 10 ) / 10}</h3>
+        <h3 className="release-date">Release Date: {movieDetails.release_date}</h3>
+        <h3 className="overview">Movie Overview: {movieDetails.overview}</h3>
+        <h3 className="genres">Genres: {movieDetails.genres}</h3>
+        <h3 className="budget">Movie Budget: {movieDetails.budget}</h3>
+        <h3 className="revenue">Movie Revenue: {movieDetails.revenue}</h3>
+        <h3 className="runTime">Movie Length: {movieDetails.runTime}</h3>
+        <h3 className="tagline">Movie Tagline: {movieDetails.tagline}</h3>
+        </div>
       <button type="button" className="return-to-home-button" onClick={() => {returnToHome()}}>Return To All Movies</button>
     </section>
   )

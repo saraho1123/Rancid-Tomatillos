72073ad4f91b7b5a5c586c9321849ad4ade6fed5
Chadrick Dickerson <chad.alan.dickerson@gmail.com>
2020-12-04 18:18:38 -0700

    Remove Movie Title from inner text in h3 for movie-title and increase the font size

--- src/MovieCard/MovieCard.js
+++ src/MovieCard/MovieCard.js
@@ -7,7 +7,7 @@ const MovieCard = ( {id, image, title, rating, displayMovieDetails}) => {
       <div className="image-holder">
         <img className="card-image" src={image} alt={title}/>
       </div>
-      <h3 className="card-title">Movie Title: {title}</h3>
+      <h3 className="card-title">{title}</h3>
       <h3 className="rating">Movie Rating: {Math.round( rating * 10 ) / 10}</h3>
     </section>
   )

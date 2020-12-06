c8bbdaa89f37f9084b03af7618835a4323c7e123
Sarah Osgood <sarahjs117@gmail.com>
2020-12-05 19:31:24 -0700

    Add tests for movie card component

--- src/MovieCard/MovieCard.test.js
+++ src/MovieCard/MovieCard.test.js
@@ -1,14 +1,46 @@
-import { render, screen, fireEvent } from '@testing-library/react';
-import '@testing-library/jest-dom'
+import React from 'react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
 import MovieCard from './MovieCard';
+import { getAllMoviesData, getSingleMovieData } from '../apiCalls.js'
+jest.mock('../apiCalls.js');
+
 
 describe('MovieCard', () => {
-  it('should return a movie card', () => {
-    // setup
-    // render of SingleCard, with mocked, id, image, title, rating, and displayMovieDetails
-    // execution
-    // 
-    // assertion
-    // we need assert that title and rating and an image are in the document
+  it('should correctly render a movie card', () => {
+    // SETUP
+    render(<MovieCard 
+      id={1234}
+      image='https://someimage.png'
+      title='The Best Movie'
+      rating='10'
+      getMovieDetails={jest.fn()}
+      />)
+
+    // EXECUTION
+    const sampleMovieCardTitle = screen.getByText('The Best Movie');
+
+    // ASSERTION
+    expect(sampleMovieCardTitle).toBeInTheDocument();
+  })
+
+  it('should call displayMovieDetails with correct id', () => {      
+    // SETUP
+    const mockDisplayMovieDetails = jest.fn();
+    render(<MovieCard 
+      id={1234}
+      image='https://someimage.png'
+      title='The Best Movie'
+      rating='10'
+      getMovieDetails={mockDisplayMovieDetails}
+      />)
+
+    // EXECUTION
+    const displayMovieCard = screen.getByAltText('The Best Movie')
+    fireEvent.click(displayMovieCard)
+
+    // ASSERTION
+    expect(mockDisplayMovieDetails).toHaveBeenCalledWith(1234);
   })
 })

32c636ed0f772c6c1f00c54fbd83ed4de5417596
Chadrick Dickerson <chad.alan.dickerson@gmail.com>
2020-12-07 15:19:21 -0700

    Remove commented text from Header.js

--- src/Header/Header.test.js
+++ src/Header/Header.test.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import '@testing-library/jest-dom';
-import { screen, render, fireEvent } from '@testing-library/react';
+import { screen, render } from '@testing-library/react';
 import Header from './Header';
 
 describe('Header', () => {
   it('should render a header when page loads', () => {
-    // setup
     render(
       <Header/>
     )
-    // assertion
+   
     const heading = screen.getByText('Rancid Tomatillos Movie Ratings');
     
     expect(heading).toBeInTheDocument();
-    // we want to look for 'Rancid Tomatillos Movie ratiangs' in the document
   })
 })

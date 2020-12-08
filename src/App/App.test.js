c7b65d2e316240c142e94e2c9565c363b7f16ca8
Sarah Osgood <sarahjs117@gmail.com>
2020-12-07 19:12:53 -0700

    Add async test for displaying movies on screen

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -7,8 +7,51 @@ jest.mock('../apiCalls.js');
 // no need to import other components for integration testing as app already has those components imported and the child components that were imported into its children
 
 describe('App', () => {
-  it('should take user to movie cards view when return to home button is clicked', () => {
+
+  it('should dispay movie cards on page load', async () => {
     // setup
+    const mockComponentDidMount = jest.fn();
+    getAllMoviesData.mockResolvedValueOnce(
+      {'movies': [
+        {
+          "id": 694919,
+          "poster_path": "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg",
+          "backdrop_path": "https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg",
+          "title": "Money Plane",
+          "average_rating": 6.666666666666667,
+          "release_date": "2020-09-29"
+        },
+        {
+          "id": 337401,
+          "poster_path": "https://image.tmdb.org/t/p/original//aKx1ARwG55zZ0GpRvU2WrGrCG9o.jpg",
+          "backdrop_path": "https://image.tmdb.org/t/p/original//zzWGRw277MNoCs3zhyG3YmYQsXv.jpg",
+          "title": "Mulan",
+          "average_rating": 4.909090909090909,
+          "release_date": "2020-09-04"
+        },
+        {
+          "id": 718444,
+          "poster_path": "https://image.tmdb.org/t/p/original//uOw5JD8IlD546feZ6oxbIjvN66P.jpg",
+          "backdrop_path": "https://image.tmdb.org/t/p/original//x4UkhIQuHIJyeeOTdcbZ3t3gBSa.jpg",
+          "title": "Rogue",
+          "average_rating": 6.428571428571429,
+          "release_date": "2020-08-20"
+        }
+      ]}
+    )
+    screen.debug
+
+    render(<App />)
+
+
+    const moneyPlaneCard = await waitFor(() => screen.getByText('Money Plane'))
+    const mulanCard = await waitFor(() => screen.getByText('Mulan'))
+    const rogueCard = await waitFor(() => screen.getByText('Rogue'))
+
+    expect(moneyPlaneCard).toBeInTheDocument();
+    expect(mulanCard).toBeInTheDocument();
+    expect(rogueCard).toBeInTheDocument();
+          
     // render app with all components, with singleMovie displayed and mocked returntohome
     // execution
     // fireEvent or userEvent for onclick of Return to home button
@@ -18,6 +61,7 @@ describe('App', () => {
 
  it('should take user to single movie when card is clicked', () => {
   // setup
+
    // render of SingleCard, with mocked, id, image, title, rating, and displayMovieDetails
   // execution
    // fireEvent or userEvent for onclick of MovieCard

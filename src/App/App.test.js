a96e6fae0ac0e660cd49961ca40dc380e938f9eb
Sarah Osgood <sarahjs117@gmail.com>
2020-12-07 19:01:14 -0700

    Save test for return to home so we can try another test

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -25,43 +25,67 @@ describe('App', () => {
    // expect movieContainer to singleMovie
   })  
 
-  it('should return user to home when Return To All Movies button is clicked from a single movie', async () => {
-    const mockGetAllMoviesData = jest.fn()
-    // need to mock single movies!!
-    getAllMoviesData.mockResolvedValueOnce([
-      {
-        "id": 694919,
-        "poster_path": "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg",
-        "backdrop_path": "https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg",
-        "title": "Money Plane",
-        "average_rating": 6.666666666666667,
-        "release_date": "2020-09-29"
-      },
-      {
-        "id": 337401,
-        "poster_path": "https://image.tmdb.org/t/p/original//aKx1ARwG55zZ0GpRvU2WrGrCG9o.jpg",
-        "backdrop_path": "https://image.tmdb.org/t/p/original//zzWGRw277MNoCs3zhyG3YmYQsXv.jpg",
-        "title": "Mulan",
-        "average_rating": 4.909090909090909,
-        "release_date": "2020-09-04"
-      },
-      {
-        "id": 718444,
-        "poster_path": "https://image.tmdb.org/t/p/original//uOw5JD8IlD546feZ6oxbIjvN66P.jpg",
-        "backdrop_path": "https://image.tmdb.org/t/p/original//x4UkhIQuHIJyeeOTdcbZ3t3gBSa.jpg",
-        "title": "Rogue",
-        "average_rating": 6.428571428571429,
-        "release_date": "2020-08-20"
-      }
-    ])
-    render(<App />)
+  // it('should return user to home when Return To All Movies button is clicked from a single movie', async () => {
+  //   // need to mock single movies!!
+  //   getAllMoviesData.mockResolvedValueOnce(
+  //     {'movies': [
+  //       {
+  //         "id": 694919,
+  //         "poster_path": "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg",
+  //         "backdrop_path": "https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg",
+  //         "title": "Money Plane",
+  //         "average_rating": 6.666666666666667,
+  //         "release_date": "2020-09-29"
+  //       },
+  //       {
+  //         "id": 337401,
+  //         "poster_path": "https://image.tmdb.org/t/p/original//aKx1ARwG55zZ0GpRvU2WrGrCG9o.jpg",
+  //         "backdrop_path": "https://image.tmdb.org/t/p/original//zzWGRw277MNoCs3zhyG3YmYQsXv.jpg",
+  //         "title": "Mulan",
+  //         "average_rating": 4.909090909090909,
+  //         "release_date": "2020-09-04"
+  //       },
+  //       {
+  //         "id": 718444,
+  //         "poster_path": "https://image.tmdb.org/t/p/original//uOw5JD8IlD546feZ6oxbIjvN66P.jpg",
+  //         "backdrop_path": "https://image.tmdb.org/t/p/original//x4UkhIQuHIJyeeOTdcbZ3t3gBSa.jpg",
+  //         "title": "Rogue",
+  //         "average_rating": 6.428571428571429,
+  //         "release_date": "2020-08-20"
+  //       }
+  //     ]}
+  //   )
 
-    const movieCard = screen.getByText('Rogue')
-    fireEvent.click(movieCard)
-    const returnButton = await waitFor(() => screen.getByText('Return To All Movies'))
-    fireEvent.click(returnButton)
+  //   getSingleMovieData.mockResolvedValueOnce({
+  //       "id": 718444,
+  //       "title": "Rogue",
+  //       "poster_path": "https://image.tmdb.org/t/p/original//uOw5JD8IlD546feZ6oxbIjvN66P.jpg",
+  //       "backdrop_path": "https://image.tmdb.org/t/p/original//x4UkhIQuHIJyeeOTdcbZ3t3gBSa.jpg",
+  //       "release_date": "2020-08-20",
+  //       "overview": "Battle-hardened O’Hara leads a lively mercenary team of soldiers on a daring mission: rescue hostages from their captors in remote Africa. But as the mission goes awry and the team is stranded, O’Hara’s squad must face a bloody, brutal encounter with a gang of rebels.",
+  //       "genres": [
+  //           "Action"
+  //       ],
+  //       "budget": 0,
+  //       "revenue": 0,
+  //       "runtime": 106,
+  //       "tagline": "When the hunter becomes the prey.",
+  //       "average_rating": 6.428571428571429,
+  //     })
+    
+  //   render(<App />)
+    
+  //   const movieCardRogue = await waitFor(() => screen.getByText('Rogue'))
+    
+  //   fireEvent.click(movieCardRogue)
+    
+  //   screen.debug
+  //   const returnButton = await waitFor(() => screen.getByRole('button', { name: /return to all movies/i }))
+    
+  //   fireEvent.click(returnButton)
 
-    expect(screen.getByText('Money Plane')).toBeInTheDocument()
-    expect(returnButton).not.toBeInTheDocument()
-  })
+  //   expect(screen.getByText('Money Plane')).toBeInTheDocument()
+  //   expect(returnButton).not.toBeInTheDocument()
+  // })
+  
 });

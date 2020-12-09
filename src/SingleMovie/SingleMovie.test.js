b65c668ea8babd458b05fe7d145a2726924e9c04
Sarah Osgood <sarahjs117@gmail.com>
2020-12-09 16:35:31 -0700

    Refactor unit test for return button using memory history

--- src/SingleMovie/SingleMovie.test.js
+++ src/SingleMovie/SingleMovie.test.js
@@ -3,7 +3,9 @@ import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import SingleMovie from './SingleMovie';
 import { MemoryRouter } from 'react-router';
-import { getSingleMovieData } from '../apiCalls.js'
+import { getSingleMovieData } from '../apiCalls.js';
+import userEvent from '@testing-library/user-event';
+import { createMemoryHistory } from "history";
 jest.mock('../apiCalls.js');
 
 
@@ -47,38 +49,38 @@ describe('SingleMovie', () => {
     expect(overview).toBeInTheDocument();
   }) 
   
-//   it('should invoke return to home method when Return To Home button is clicked', async () => {
-//     getSingleMovieData.mockResolvedValueOnce({
-//       "movie": {
-//         "id": 694919,
-//         "title": "Money Plane",
-//         "poster_path": "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg",
-//         "backdrop_path": "https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg",
-//         "release_date": "2020-09-29",
-//         "overview": "A professional thief with $40 million in debt and his family's life on the line must commit one final heist - rob a futuristic airborne casino filled with the world's most dangerous criminals.",
-//         "genres": [
-//           "Action"
-//         ],
-//         "budget": 0,
-//         "revenue": 0,
-//         "runtime": 82,
-//         "tagline": "",
-//         "average_rating": 6.666666666666667
-//       }
-//     })
+  it('should invoke return to home method when Return To Home button is clicked', async () => {
+    const history = createMemoryHistory()
+    getSingleMovieData.mockResolvedValueOnce({
+      "movie": {
+        "id": 694919,
+        "title": "Money Plane",
+        "poster_path": "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg",
+        "backdrop_path": "https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg",
+        "release_date": "2020-09-29",
+        "overview": "A professional thief with $40 million in debt and his family's life on the line must commit one final heist - rob a futuristic airborne casino filled with the world's most dangerous criminals.",
+        "genres": [
+          "Action"
+        ],
+        "budget": 0,
+        "revenue": 0,
+        "runtime": 82,
+        "tagline": "",
+        "average_rating": 6.666666666666667
+      }
+    })
       
-//     render(
-//       <MemoryRouter>
-//         <SingleMovie
-//           id="694919"
-//           />
-//       </MemoryRouter>
-//     )
- 
-//     const returnButton = await waitFor(() => screen.getByText('◀Return to All Movies'));
+    render(
+      <MemoryRouter history={history}>
+        <SingleMovie
+          id="694919"
+          />
+      </MemoryRouter>
+    )
+  
+    const returnButton = await waitFor(() => screen.getByText('◀Return to All Movies'));
+    userEvent.click(returnButton)
 
-//     fireEvent.click(returnButton);
-
-//     expect(returnButton).not.toBeInTheDocument();
-//  })
+    expect(history.location.pathname).toBe('/')
+  })
 })

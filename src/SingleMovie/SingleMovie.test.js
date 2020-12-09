556c4543d39587ba84651f714501a6ebe6289f8d
Sarah Osgood <sarahjs117@gmail.com>
2020-12-09 15:48:26 -0700

    Refactor SingleMovie tests to account for using router but we may need the commented out unit test for the return button

--- src/SingleMovie/SingleMovie.test.js
+++ src/SingleMovie/SingleMovie.test.js
@@ -1,76 +1,84 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
-import ReactDOM from 'react-dom';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import SingleMovie from './SingleMovie';
+import { MemoryRouter } from 'react-router';
+import { getSingleMovieData } from '../apiCalls.js'
+jest.mock('../apiCalls.js');
+
 
 describe('SingleMovie', () => {
-  it('should render details for a single movie', () => {
-    const movie = {
-      "id": 694919,
-      "title": "Money Plane",
-      "poster_path": "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg",
-      "backdrop_path": "https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg",
-      "release_date": "2020-09-29",
-      "overview": "A professional thief with $40 million in debt and his family's life on the line must commit one final heist - rob a futuristic airborne casino filled with the world's most dangerous criminals.",
-      "genres": [
-        "Action"
-      ],
-      "budget": 0,
-      "revenue": 0,
-      "runtime": 82,
-      "tagline": "",
-      "average_rating": 6.666666666666667
-    }
+  it('should render details for a single movie', async () => {
+    getSingleMovieData.mockResolvedValueOnce({
+      "movie": {
+        "id": 694919,
+        "title": "Money Plane",
+        "poster_path": "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg",
+        "backdrop_path": "https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg",
+        "release_date": "2020-09-29",
+        "overview": "A professional thief with $40 million in debt and his family's life on the line must commit one final heist - rob a futuristic airborne casino filled with the world's most dangerous criminals.",
+        "genres": [
+          "Action"
+        ],
+        "budget": 0,
+        "revenue": 0,
+        "runtime": 82,
+        "tagline": "",
+        "average_rating": 6.666666666666667
+      }
+    })
   
    const mockReturnToHome = jest.fn();
     
     render(
-      <SingleMovie
-        movieDetails={movie}
-        returnToHome={mockReturnToHome}
-      />
+      <MemoryRouter>
+        <SingleMovie
+          id="694919"
+          returnToHome={mockReturnToHome}
+          />
+      </MemoryRouter>
     )
 
-    const image = screen.getByRole('img', { name: /money plane/i });
-    const title = screen.getByRole('heading', { name: /money plane/i });
-    const overview = screen.getByRole('heading', { name: /overview: a professional thief with \$40 million in debt and his family's life on the line must commit one final heist \- rob a futuristic airborne casino filled with the world's most dangerous criminals\./i });
+    const image = await waitFor(() => screen.getByRole('img', { name: /money plane/i }));
+    const title = await waitFor(() => screen.getByRole('heading', { name: /money plane/i }));
+    const overview = await waitFor(() => screen.getByRole('heading', { name: /overview: a professional thief with \$40 million in debt and his family's life on the line must commit one final heist \- rob a futuristic airborne casino filled with the world's most dangerous criminals\./i }));
     expect(image).toBeInTheDocument();
     expect(title).toBeInTheDocument();
     expect(overview).toBeInTheDocument();
   }) 
   
-  it('should invoke return to home method when Return To Home button is clicked', () => {
-    const movie = {
-      "id": 694919,
-      "title": "Money Plane",
-      "poster_path": "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg",
-      "backdrop_path": "https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg",
-      "release_date": "2020-09-29",
-      "overview": "A professional thief with $40 million in debt and his family's life on the line must commit one final heist - rob a futuristic airborne casino filled with the world's most dangerous criminals.",
-      "genres": [
-        "Action"
-      ],
-      "budget": 0,
-      "revenue": 0,
-      "runtime": 82,
-      "tagline": "",
-      "average_rating": 6.666666666666667
-    }
-  
-    const mockReturnToHome = jest.fn();
-
-    render(
-    <SingleMovie
-      movieDetails={movie}
-      returnToHome={mockReturnToHome}
-    />
-    )
+//   it('should invoke return to home method when Return To Home button is clicked', async () => {
+//     getSingleMovieData.mockResolvedValueOnce({
+//       "movie": {
+//         "id": 694919,
+//         "title": "Money Plane",
+//         "poster_path": "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg",
+//         "backdrop_path": "https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg",
+//         "release_date": "2020-09-29",
+//         "overview": "A professional thief with $40 million in debt and his family's life on the line must commit one final heist - rob a futuristic airborne casino filled with the world's most dangerous criminals.",
+//         "genres": [
+//           "Action"
+//         ],
+//         "budget": 0,
+//         "revenue": 0,
+//         "runtime": 82,
+//         "tagline": "",
+//         "average_rating": 6.666666666666667
+//       }
+//     })
+      
+//     render(
+//       <MemoryRouter>
+//         <SingleMovie
+//           id="694919"
+//           />
+//       </MemoryRouter>
+//     )
  
-    const returnButton = screen.getByText('Return To All Movies');
+//     const returnButton = await waitFor(() => screen.getByText('◀Return to All Movies'));
 
-    fireEvent.click(returnButton);
+//     fireEvent.click(returnButton);
 
-    expect(mockReturnToHome).toHaveBeenCalledWith();
- })
+//     expect(returnButton).not.toBeInTheDocument();
+//  })
 })

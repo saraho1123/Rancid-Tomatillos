e7e52c5667c10a90afe1df13834e3b3d2e1bc36a
Chadrick Dickerson <chad.alan.dickerson@gmail.com>
2020-12-08 18:34:05 -0700

    Add Link to MovieCard

--- src/MovieCard/MovieCard.js
+++ src/MovieCard/MovieCard.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import './MovieCard.css';
+import { Link } from 'react-router-dom';
 
-const MovieCard = ( {id, image, title, rating, getMovieDetails}) => {
+const MovieCard = ( {id, image, title, rating }) => {
   return (
-    <section className="movie-card" onClick={() => {getMovieDetails(id)} }>
-      <div className="image-holder">
-        <img className="card-image" src={image} alt={title}/>
-      </div>
-      <h3 className="card-title">{title}</h3>
-      <h3 className="rating">Rating: {Math.round( rating * 10 ) / 10}</h3>
-    </section>
+    <Link to={`/${id}`} >
+      <section className="movie-card" >
+        <div className="image-holder">
+          <img className="card-image" src={image} alt={title}/>
+        </div>
+        <h3 className="card-title">{title}</h3>
+        <h3 className="rating">Rating: {Math.round( rating * 10 ) / 10}</h3>
+      </section>
+    </Link>
   )
 }
 
 export default MovieCard
+
+// onClick={() => {getMovieDetails(id)}

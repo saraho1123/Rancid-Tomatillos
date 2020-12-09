33afc6f3288e36491dcd3740646befd6de7efd25
Sarah Osgood <sarahjs117@gmail.com>
2020-12-08 19:10:49 -0700

    Add new styling for the route links

--- src/MovieCard/MovieCard.js
+++ src/MovieCard/MovieCard.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 const MovieCard = ( {id, image, title, rating }) => {
   return (
-    <Link to={`/${id}`} >
+    <Link to={`/${id}`} className="movie-card-link">
       <section className="movie-card" >
         <div className="image-holder">
           <img className="card-image" src={image} alt={title}/>

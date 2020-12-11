24aa0d4bae761d7c75688aa93a0b87014a21408a
Sarah Osgood <sarahjs117@gmail.com>
2020-12-11 11:02:03 -0700

    Add to error message that is displayed for user

--- src/Error/Error.js
+++ src/Error/Error.js
@@ -3,7 +3,7 @@ import './Error.css';
 
 const Error = ({ error }) => {
   return(
-    <h1>{error}</h1>
+    <h1 className="error-message">{error} <br/> Please refresh the page or try again later.</h1>
   )
 } 
 

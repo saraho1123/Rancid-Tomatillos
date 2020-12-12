d8e46f82ac1798629fa6f84da096a7d2008a211d
Sarah Osgood <sarahjs117@gmail.com>
2020-12-12 15:30:30 -0700

    Update styling for accessibility

--- src/MovieCard/MovieCard.js
+++ src/MovieCard/MovieCard.js
@@ -9,7 +9,7 @@ const MovieCard = ( {id, image, title, rating }) => {
         <div className="image-holder">
           <img className="card-image" src={image} alt={title}/>
         </div>
-        <h3 className="card-title">{title}</h3>
+        <h2 className="card-title">{title}</h2>
         <h3 className="rating">Rating: {Math.round( rating * 10 ) / 10}</h3>
       </section>
     </Link>

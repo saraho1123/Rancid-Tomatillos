e28b983b57256174c7fd6e716fa06de01d2d6a7f
Chadrick Dickerson <chad.alan.dickerson@gmail.com>
2020-12-14 16:39:34 -0700

    Write and pass integration testing for RatingSearch and MoviesContainer within App

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -7,7 +7,7 @@ import { MemoryRouter } from 'react-router';
 jest.mock('../apiCalls.js');
 
 describe('App', () => {
-  it('should dispay movie cards on page load', async () => {
+  beforeEach(() => {
     getAllMoviesData.mockResolvedValueOnce(
       {'movies': [
         {
@@ -36,7 +36,9 @@ describe('App', () => {
         }
       ]}
     )
+  })
 
+  it('should dispay movie cards on page load', async () => {
     render(
     <MemoryRouter>
       <App />
@@ -53,35 +55,6 @@ describe('App', () => {
  })
 
   it('should take user to single movie when card is clicked', async () => {
-    getAllMoviesData.mockResolvedValueOnce(
-      {'movies': [
-        {
-          "id": 694919,
-          "poster_path": "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg",
-          "backdrop_path": "https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg",
-          "title": "Money Plane",
-          "average_rating": 6.666666666666667,
-          "release_date": "2020-09-29"
-        },
-        {
-          "id": 337401,
-          "poster_path": "https://image.tmdb.org/t/p/original//aKx1ARwG55zZ0GpRvU2WrGrCG9o.jpg",
-          "backdrop_path": "https://image.tmdb.org/t/p/original//zzWGRw277MNoCs3zhyG3YmYQsXv.jpg",
-          "title": "Mulan",
-          "average_rating": 4.909090909090909,
-          "release_date": "2020-09-04"
-        },
-        {
-          "id": 718444,
-          "poster_path": "https://image.tmdb.org/t/p/original//uOw5JD8IlD546feZ6oxbIjvN66P.jpg",
-          "backdrop_path": "https://image.tmdb.org/t/p/original//x4UkhIQuHIJyeeOTdcbZ3t3gBSa.jpg",
-          "title": "Rogue",
-          "average_rating": 6.428571428571429,
-          "release_date": "2020-08-20"
-        }
-      ]}
-    )
-
     getSingleMovieData.mockResolvedValueOnce({
       "movie": {
         "id": 718444,
@@ -115,35 +88,6 @@ describe('App', () => {
   })
 
   it('should return user to home when Return To All Movies button is clicked from a single movie', async () => {
-    getAllMoviesData.mockResolvedValueOnce(
-      {'movies': [
-        {
-          "id": 694919,
-          "poster_path": "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg",
-          "backdrop_path": "https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg",
-          "title": "Money Plane",
-          "average_rating": 6.666666666666667,
-          "release_date": "2020-09-29"
-        },
-        {
-          "id": 337401,
-          "poster_path": "https://image.tmdb.org/t/p/original//aKx1ARwG55zZ0GpRvU2WrGrCG9o.jpg",
-          "backdrop_path": "https://image.tmdb.org/t/p/original//zzWGRw277MNoCs3zhyG3YmYQsXv.jpg",
-          "title": "Mulan",
-          "average_rating": 4.909090909090909,
-          "release_date": "2020-09-04"
-        },
-        {
-          "id": 718444,
-          "poster_path": "https://image.tmdb.org/t/p/original//uOw5JD8IlD546feZ6oxbIjvN66P.jpg",
-          "backdrop_path": "https://image.tmdb.org/t/p/original//x4UkhIQuHIJyeeOTdcbZ3t3gBSa.jpg",
-          "title": "Rogue",
-          "average_rating": 6.428571428571429,
-          "release_date": "2020-08-20"
-        }
-      ]}
-    )
-
     getSingleMovieData.mockResolvedValueOnce({
       "movie": {
         "id": 718444,
@@ -181,4 +125,47 @@ describe('App', () => {
     expect(returnButton).not.toBeInTheDocument()
   })
   
+  it('should render only the cards within range selected by user from rating search', async () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+      )
+
+      const dropdownButton = screen.getByText('Search Movies By Rating');
+
+      fireEvent.click(dropdownButton);
+
+      const optionDropdownText = await waitFor(() => screen.getByText('6-7'));
+  
+      fireEvent.click(optionDropdownText);
+
+      const title1 = await waitFor(() => screen.getByText('Money Plane'));
+      const title2 = await waitFor(() => screen.getByText('Rogue'));
+      const title3 = await waitFor(() => screen.queryByText('Mulan'));
+
+      expect(title1).toBeInTheDocument();
+      expect(title2).toBeInTheDocument();
+      expect(title3).toBeNull();
+  })
+
+  it('should render a message when no movies by chosen rating are available', async () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+      )
+
+      const dropdownButton = screen.getByText('Search Movies By Rating');
+
+      fireEvent.click(dropdownButton);
+
+      const optionDropdownText = await waitFor(() => screen.getByText('1-2'));
+  
+      fireEvent.click(optionDropdownText);
+
+      const message = screen.getByRole('heading', { name: /there are no movies with this rating range\. please choose a different range\./i });
+    
+      expect(message).toBeInTheDocument();
+  })
 });

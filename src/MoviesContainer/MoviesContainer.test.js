b0559e7b2515c230069ab74c0668b244582bb8de
Chadrick Dickerson <chad.alan.dickerson@gmail.com>
2020-12-14 15:45:47 -0700

    Remove fireEvent from imports

--- src/MoviesContainer/MoviesContainer.test.js
+++ src/MoviesContainer/MoviesContainer.test.js
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import MoviesContainer from './MoviesContainer';
 import { MemoryRouter } from 'react-router';
